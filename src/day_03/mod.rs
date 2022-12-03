9212252dd011d3b0d2a927a1e782f479cb91e8c9
Martin Koščak <martin.kosca@posta.si>
2022-12-03 20:03:45 +0100

    Completed solution for day 3.

--- src/day_03/mod.rs
+++ src/day_03/mod.rs
@@ -1,5 +1,3 @@
-use std::ops::Index;
-
 pub struct RucksackReorganization {
     data: Vec<(String, String)>
 }
@@ -14,16 +12,38 @@ fn get_common_char(lhs: &String, rhs: &String) -> Option<char> {
     None
 }
 
-let lowercase: Vec<char> = ('a'..='z').into_iter().collect();
-let uppercase: Vec<char> = ('A'..='Z').into_iter().collect();
+fn get_common_chars(items: &Vec<String>) -> Option<char> { 
+    let mut remaining_chars: Vec<char> = items.first().unwrap().chars().collect();
+    remaining_chars.sort_unstable();
+    remaining_chars.dedup();
+    
+
+    for item in &items[1..] {
+        let mut new_chars: Vec<char> = vec![];
+
+        for c in &remaining_chars {
+            if item.chars().any(|el| *c == el) {
+                new_chars.push(*c);
+            }
+        }
+
+        remaining_chars.clear();
+        remaining_chars = new_chars.clone();
+    }
+
+    if remaining_chars.len() == 1 {
+        return Some(remaining_chars[0]);
+    }
+
+    None
+}
 
 fn char_priority(c: char) -> usize {
     let priority = if c.is_ascii_lowercase() {
-        LOWERCASE.iter().position(|el| *el == c).unwrap()
+        ('a'..='z').into_iter().position(|el| el == c).unwrap() + 1
     } else {
-        UPPERCASE.iter().position(|el| *el == c).unwrap() + 26
+        ('A'..='Z').into_iter().position(|el| el == c).unwrap() + 27
     };
-
     priority
 }
 
@@ -46,11 +66,28 @@ impl crate::Advent for RucksackReorganization {
                 common_chars.push(c)
             }
         }
-        println!("Common chars: {:?}", common_chars);
-        1
+        common_chars.iter()
+            .map(|c| char_priority(*c))
+            .sum::<usize>()
     }
     
     fn part_02(&self) -> usize {
-        2
+        let mut common_chars: Vec<char> = vec![];
+        for items in self.data.chunks(3) {
+            let lines: Vec<String> = items
+                .iter()
+                .map(|(lhs, rhs)| {
+                    let mut new_str = String::from(lhs);
+                    new_str.push_str(rhs);
+                    new_str
+                })
+                .collect();
+            let common_char = get_common_chars(&lines);
+            common_chars.push(common_char.unwrap())            
+        }
+
+        common_chars.iter()
+            .map(|c| char_priority(*c))
+            .sum::<usize>()        
     }
 }
