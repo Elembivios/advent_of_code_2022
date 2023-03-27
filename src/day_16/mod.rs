1794c3a96000ee7d4d0a527817ba4e1c43cab7b9
Martin KOŠČAK <koscak@pops.pri>
2023-03-27 15:08:42 +0200

    Added both mine and copied solution from reddit for day 16.

--- src/day_16/mod.rs
+++ src/day_16/mod.rs
@@ -1,57 +1,261 @@
-use std::collections::HashMap;
-use anyhow::{Error, Context};
-use std::str::FromStr;
 
+// Copied code from: https://github.com/Crazytieguy/advent-of-code/blob/master/2022/src/bin/day16/main.rs
+// My solution doesn't run this fast xD
+
+use std::{collections::HashMap, cmp::Reverse};
+
+use itertools::Itertools;
+
+type Valve<'a> = (&'a str, u8, Vec<&'a str>);
+type FlowRates = Vec<u8>;
+type FlowRateIndices = Vec<usize>;
+type ShortesPathLenghts = Vec<Vec<u8>>;
+
+#[derive(Debug)]
 pub struct ProboscideaVolcanium {
-    valves: Vec<Valve>,
-    map: HashMap<String, Vec<String>>
-}
-
-#[derive(Debug, Clone)]
-struct Valve {
-    name: String,
-    flow_rate: u32,
-}
-
-impl FromStr for Valve {
-    type Err = Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let s = s.strip_prefix("Valve ").context("Invalid valve string")?;
-        let (name, rest) = s.split_at(2);
-        let val = rest.strip_prefix(" has flow rate=").context("Invalid valve string")?.parse().context("Can't parse value to integer")?;
-        Ok(Valve {
-            name: name.to_string(),
-            flow_rate: val
-        })
-    }
+    // valves: Vec<Valve<'a>>,
+    flow_rates: FlowRates,
+    shortest_path_lengths: ShortesPathLenghts,
+    flow_rate_indices: FlowRateIndices,
+    starting_node: usize
 }
 
 impl crate::Advent for ProboscideaVolcanium {
-    fn new(data: &str) -> Self
-        where 
-            Self: Sized {
-        
-        let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    fn new(data: &str) -> Self {
         let valves: Vec<Valve> = data.lines().map(|l| {
             let (lhs, rhs) = l.split_once("; ").unwrap();
-            let valve = Valve::from_str(lhs).unwrap();
-            let leads_to: Vec<String> = rhs.strip_prefix("tunnels lead to valves ").unwrap().split(", ").map(|s| s.to_owned()).collect();
+            let lhs = lhs.strip_prefix("Valve ").unwrap();
+            let (name, rest) = lhs.split_at(2);
+            let flow_rate = rest.strip_prefix(" has flow rate=").unwrap().parse().unwrap();
+            let tunnels: Vec<_> = rhs
+                .strip_prefix("tunnels lead to valves ")
+                .or(rhs.strip_prefix("tunnel leads to valve "))
+                .unwrap().split(", ").collect();
+            (name, flow_rate, tunnels)
 
-            map.insert(valve.name.clone(), leads_to);
-            valve
         }).collect();
 
-        ProboscideaVolcanium { valves, map }
+        let shortest_path_lengths_uncompressed = floyd_warshall(&valves);
+
+        let interesting_valve_indices: Vec<_> = valves
+            .iter()
+            .enumerate()
+            .filter(|(_, v)| v.0 == "AA" || v.1 > 0)
+            .map(|(i, _)| i)
+            .collect();
+
+        let flow_rates: Vec<_> = interesting_valve_indices
+            .iter()
+            .map(|i| valves[*i].1)
+            .collect();
+
+        let shortest_path_lengths: Vec<Vec<_>> = interesting_valve_indices
+            .iter()
+            .map(|&i| {
+                interesting_valve_indices
+                    .iter()
+                    .map(|&j| {
+                        shortest_path_lengths_uncompressed[i][j]
+                    }).collect()
+            }).collect();
+
+        let starting_node = interesting_valve_indices
+            .iter()
+            .position(|&i| valves[i].0 == "AA")
+            .unwrap();
+
+        let sorted_flow_rate_indices: Vec<_> = flow_rates
+            .iter()
+            .enumerate()
+            .sorted_unstable_by_key(|&(_, &flow)| Reverse(flow))
+            .map(|(i, _)| i)
+            .collect();
+                
+        Self { 
+            flow_rates, 
+            shortest_path_lengths, 
+            flow_rate_indices: sorted_flow_rate_indices, 
+            starting_node
+        }
     }
 
     fn part_01(&self) -> String {
-        let mut valves = self.valves.clone();
-        valves.sort_by(|a, b| { a.flow_rate.cmp(&b.flow_rate)});
-
-        1.to_string()
+        let mut best = 0;
+        branch_and_bound(
+            &self.flow_rates, 
+            &self.flow_rate_indices, 
+            &self.shortest_path_lengths, 
+            State::new(self.starting_node as u8, 30),
+            &mut [], 
+            &mut best, 
+            |bound, best| bound > best
+        );
+        best.to_string()
     }
 
     fn part_02(&self) -> String {
-        2.to_string()
+        let mut best_per_visited = vec![0; u16::MAX as usize];
+        branch_and_bound(
+            &self.flow_rates, 
+            &self.flow_rate_indices, 
+            &self.shortest_path_lengths, 
+            State::new(self.starting_node as u8, 26),
+            &mut best_per_visited, 
+            &mut 0,
+            // This could techically produce an incorrect result, 
+            // but it doesn't on my input
+            |bound, best| bound > best * 3 / 4
+        );
+
+        let best_per_visited_filtered_sorted: Vec<_> = best_per_visited
+            .into_iter()
+            .enumerate()
+            .filter(|&(_, best)| best > 0)
+            .map(|(i, best)| (i as u16, best))
+            .sorted_unstable_by_key(|&(_, best)| Reverse(best))
+            .collect();
+
+        let mut best = 0;
+
+        for (i, &(my_visited, my_best)) in best_per_visited_filtered_sorted.iter().enumerate() {
+            for &(elephant_visited, elephant_best) in &best_per_visited_filtered_sorted[i + 1..] {
+                let score = my_best + elephant_best;
+                if score <= best {
+                    break;
+                }
+                if my_visited & elephant_visited == 0 {
+                    best = score;
+                    break;
+                }
+            }
+        }
+
+        best.to_string()
+    }
+}
+
+
+fn floyd_warshall(valves: &Vec<Valve>) -> Vec<Vec<u8>> {
+    // Returns shortest paths between all nodes.
+    let valve_name_to_ids: HashMap<&str, _> = valves 
+        .iter()
+        .enumerate()
+        .map(|(i, &(name, _, _))| (name, i))
+        .collect();
+
+    let mut dist = vec![vec![u8::MAX; valves.len()]; valves.len()];
+
+    for (i, (_, _, tunnels)) in valves.iter().enumerate() {
+        for tunnel in tunnels {
+            let j = valve_name_to_ids[tunnel];
+            dist[i][j] = 1;
+        }
+    }
+
+    (0..dist.len()).for_each(|i| {
+        dist[i][i] = 0;
+    });
+
+    for k in 0..dist.len() {
+        for i in 0..dist.len() {
+            for j in 0..dist.len() {
+                let (result, overflow) = dist[i][k].overflowing_add(dist[k][j]);
+                if !overflow && dist[i][j] > result {
+                    dist[i][j] = result;
+                }
+            }
+        }
+    }
+
+    dist
+}
+
+
+fn branch_and_bound(
+    flow_rates: &FlowRates,
+    sorted_flow_rate_indices: &[usize],
+    shortest_path_lengths: &ShortesPathLenghts,
+    state: State,
+    best_for_visited: &mut [u16],
+    best: &mut u16,
+    filter_bound: impl Fn(u16, u16) -> bool + Copy
+) {
+    if let Some(cur_best) = best_for_visited.get_mut(state.visited as usize) {
+        *cur_best = state.pressure_released.max(*cur_best);
+    }
+    *best = state.pressure_released.max(*best);
+
+    let bound_branch_pairs: Vec<_> = state
+        .branch(flow_rates, shortest_path_lengths)
+        .into_iter()
+        .map(|state| (state.bound(flow_rates, sorted_flow_rate_indices), state))
+        .filter(|&(bound, _)| filter_bound(bound, *best))
+        .sorted_unstable_by_key(|(bound, _)| Reverse(*bound))
+        .map(|(_, branch)| branch)
+        .collect();
+
+    for branch in bound_branch_pairs {
+        branch_and_bound(flow_rates, sorted_flow_rate_indices, shortest_path_lengths, branch, best_for_visited, best, filter_bound);
+    }
+        
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+struct State {
+    visited: u16,
+    avoid: u16,
+    pressure_released: u16,
+    minutes_remaining: u8,
+    position: u8,
+}
+
+impl State {
+    fn new(position: u8, minutes_remaining: u8) -> Self {
+        Self {
+            visited: 0,
+            avoid: 1 << position,
+            pressure_released: 0,
+            minutes_remaining,
+            position
+        }
+    }
+
+    fn can_visit(self, i: usize) -> bool {
+        (self.visited | self.avoid) & (1 << i) == 0
+    }
+
+    // Assuming the shortest path lengths are all 1, the best
+    // solution is to visit the valves in order of descending 
+    // flow rate.
+    fn bound(self, flow_rates: &FlowRates, sorted_flow_rate_indices: &[usize]) -> u16 {
+        self.pressure_released + (0..=self.minutes_remaining)
+            .rev()
+            .step_by(2)
+            .skip(1)
+            .zip(
+                sorted_flow_rate_indices
+                    .iter()
+                    .filter(|&&i| self.can_visit(i))
+                    .map(|&i| flow_rates[i])
+            )
+            .map(|(minutes, flow)| minutes as u16 * flow as u16)
+            .sum::<u16>()
+    }
+
+    fn branch<'a> (self, flow_rates: &'a FlowRates, shortest_path_lengths: &'a ShortesPathLenghts) -> impl IntoIterator<Item=Self> + 'a {
+        shortest_path_lengths[self.position as usize]
+            .iter()
+            .enumerate()
+            .filter(move |&(destination, _distance)| self.can_visit(destination))
+            .filter_map(move |(destination, distance)| {
+                let minutes_remaining = self.minutes_remaining.checked_sub(*distance + 1)?;
+                Some(State {
+                    visited: self.visited | (1 << destination),
+                    avoid: self.avoid,
+                    pressure_released: self.pressure_released + minutes_remaining as u16 * flow_rates[destination] as u16,
+                    minutes_remaining,
+                    position: destination as u8
+                })
+            })
     }
 }
---
+++ src/day_16/mod_longest_path.rs
@@ -0,0 +1,360 @@
+// My implementation of this monster of a problem
+// Code works, but for part 2 takes too long with real input 
+// (for example it works fine).
+
+use std::{collections::{HashMap, VecDeque}, io};
+use std::cmp::Ordering;
+use anyhow::{Error, Context};
+use itertools::Itertools;
+use std::str::FromStr;
+
+// Guesses
+// 1786 -- too low
+
+pub struct ProboscideaVolcanium {
+    map: HashMap<String, Valve>
+}
+
+#[derive(Debug, Clone)]
+struct Valve {
+    name: String,
+    flow_rate: i32,
+    leads_to: Vec<String>
+}
+
+
+impl FromStr for Valve {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (lhs, rhs) = s.split_once("; ").unwrap();
+        let lhs = lhs.strip_prefix("Valve ").context("Invalid valve string")?;
+        let (name, rest) = lhs.split_at(2);
+        let val = rest.strip_prefix(" has flow rate=").context("Invalid valve string")?.parse().context("Can't parse value to integer")?;
+        let leads_to = match rhs.strip_prefix("tunnels lead to valves ") {
+            Some(rhs) => rhs.split(", ").map(|s| s.to_owned()).collect(),
+            None => {
+                let single_valve = rhs.strip_prefix("tunnel leads to valve ").unwrap();
+                vec![single_valve.to_owned()]
+            }
+        };
+        Ok(Valve {
+            name: name.to_string(),
+            flow_rate: val,
+            leads_to
+        })
+    }
+}
+
+impl crate::Advent for ProboscideaVolcanium {
+    fn new(data: &str) -> Self
+        where 
+            Self: Sized {
+        let map: HashMap<String, Valve> = data.lines().map(|l| {
+            let valve = Valve::from_str(l).unwrap();
+            
+            (valve.name.clone(), valve)
+        }).collect();
+
+        ProboscideaVolcanium { map }
+    }
+
+    fn part_01(&self) -> String {  
+        let (max_pressure, came_from) = self.get_max_pressure(30);
+        println!("Max cost: {}\nCame from: {:?}", max_pressure, came_from);
+        max_pressure.to_string()
+    }
+
+    fn part_02(&self) -> String {
+        let (pressure, came_from) = self.get_max_pressure_double(26);
+        // let (second_pressure, second_came_from) = self.get_max_pressure(26);
+        println!("Second pressure: {}\nCame from: {:?}", pressure, came_from);
+        2.to_string()
+    }
+}
+
+
+// fn cmp_cost(this: &(i32, i32), that: &(i32, i32)) -> Ordering {
+//     // Compares two vertex values. Returns Ordering::Greater if this is better than that etc.
+
+//     // Normal check (higher is better)
+//     let pressure_release_cmp = this.0.cmp(&that.0);
+//     if pressure_release_cmp.is_eq() {
+//         // Inverse check (lower is better)
+//         return this.1.cmp(&that.1).reverse();
+//     }
+//     pressure_release_cmp
+// }
+
+// fn construct_path(end_node: String, came_from: &HashMap<String, Option<String>>) -> Vec<String> {
+//     let mut current = &end_node;
+//     let mut path = vec![];
+//     while let Some(prev) = &came_from[current] {
+//         println!("{} -> {}", current, prev);
+//         path.push(prev.clone());
+//         if prev == "AA" {
+//             return path;
+//         }
+//         current = prev;
+//     }
+//     path
+// }
+
+fn wait_user_input() {
+    let mut s = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut s).unwrap();
+}
+
+fn sort_pair(lhs: &String, rhs: &String) -> (String, String) {
+    match lhs.cmp(&rhs) {
+        Ordering::Greater => (rhs.clone(), lhs.clone()),
+        _ => (lhs.clone(), rhs.clone())
+    }
+}
+
+impl ProboscideaVolcanium {
+    fn get_max_pressure(&self, time_limit: i32) -> (i32, Vec<String>) {
+        // cost = (sum_pressure, time_passed)
+        let mut queue: VecDeque<(String, (i32, i32), Vec<String>)> = VecDeque::new();
+        let current: String = "AA".to_owned();
+        
+        let visited: Vec<String> = vec![];
+        queue.push_back((current.clone(), (0, 0), visited));
+        let mut node_cost_map: HashMap<String, (i32, i32)> = HashMap::new();
+        let mut distances_map: HashMap<String, HashMap<String, i32>> = HashMap::new();
+        let vertices: Vec<String> = self.map.keys().sorted().cloned().collect();        
+        for vertex in vertices.iter() {
+            node_cost_map.insert(vertex.clone(), (i32::MIN, i32::MAX));
+            distances_map.insert(vertex.clone(), self.get_distances(&vertex));
+        }
+        
+        let mut max_pressure = i32::MIN;
+        let mut came_from: Vec<String> = vec![];
+        // let mut iteration = 0;
+        while let Some((u, cost, visited)) =  queue.pop_front() {                                    
+            // println!("{} {:?} -> {:?}, Cost: {:?}, Visited: {:?}", iteration, prev, u, cost, visited);            
+            // if (iteration + 1) % 10 == 0 {                                
+            //     wait_user_input();
+            // }
+
+            if cost.0 > max_pressure {
+                max_pressure = cost.0;
+                came_from = visited.clone();
+            }
+            let distance_map = &distances_map[&u];
+            for vertex in vertices.iter() {
+                if !visited.contains(&vertex) {
+                    let distance = distance_map[vertex];
+                    let (pressure_release, time_to_release_pressure) = self.get_pressure_and_time(vertex, distance, cost.1, time_limit);
+
+                    if time_to_release_pressure > time_limit {
+                        continue;
+                    }
+
+                    let mut new_visited = visited.clone();
+                    new_visited.push(vertex.clone());
+
+                    let new_tp = time_to_release_pressure;
+                    let new_pr = cost.0 + pressure_release;
+                    let new_cost = (new_pr, new_tp);
+
+                    let current_cost = node_cost_map[vertex];
+                    // if cmp_cost(&new_cost, &current_cost).is_ge() {
+                    if new_cost.0 + pressure_release >= current_cost.0 {
+                        *node_cost_map.get_mut(vertex).unwrap() = new_cost;
+                        queue.push_back((vertex.clone(), new_cost, new_visited.clone()));                        
+                    }
+                }
+            }
+            // iteration += 1;    
+        }
+        return (max_pressure, came_from)    
+    }         
+
+    fn get_max_pressure_double(&self, time_limit: i32) -> (i32, Vec<String>) {
+        // cost = (sum_pressure, time_passed)
+        let mut queue: VecDeque<((String, (i32, i32)), (String, (i32, i32)), Vec<String>)> = VecDeque::new();
+        let current: String = "AA".to_owned();
+        
+        let visited: Vec<String> = vec![];
+        queue.push_back((
+            (current.clone(), (0, 0)),
+            (current.clone(), (0, 0)),
+            visited
+        ));
+        let mut node_cost_map: HashMap<(String, String), (i32, i32)> = HashMap::new();
+        let mut distances_map: HashMap<String, HashMap<String, i32>> = HashMap::new();
+        let vertices: Vec<String> = self.map.keys().sorted().cloned().collect();
+        println!("Vertices: {:?}", vertices);
+        for vertex in vertices.iter() {            
+            distances_map.insert(vertex.clone(), self.get_distances(&vertex));
+        }
+
+        let vertices_perm: Vec<_> = vertices.iter().permutations(2).map(|vs| {
+            (vs[0].clone(), vs[1].clone())
+            // sort_pair(vs[0], vs[1])
+        }).filter(|(a, b)| a != b).collect();
+        println!("Perms: {:?}", vertices_perm);
+        println!("Perms: {:?}", vertices_perm.len());
+        
+
+        for pair in vertices_perm.iter() {
+            node_cost_map.insert(pair.clone(), (i32::MIN, i32::MAX));
+        }
+        println!("Node cost map len: {:?}", node_cost_map.len());
+
+        
+        let mut max_pressure = i32::MIN;
+        let mut came_from: Vec<String> = vec![];
+        
+        let mut iteration = 0;
+        while let Some(((u, u_cost), (i, i_cost), visited)) =  queue.pop_back() {                                                
+            if (iteration + 1) % 10000 == 0 {    
+                println!("{} {:?}({:?}) - {:?}({:?}), Visited: {:?}, queue len: {}", iteration, u, u_cost, i, i_cost, visited, queue.len());            
+                // wait_user_input();
+            }
+
+            let pressure_sum = u_cost.0 + i_cost.0;
+            if pressure_sum > max_pressure {
+                max_pressure = pressure_sum;
+                came_from = visited.clone();
+            }
+            let u_distance_map = &distances_map[&u];
+            let i_distance_map = &distances_map[&i];
+            for (u_vertex, i_vertex) in vertices_perm.iter() {
+                if !visited.contains(u_vertex) && !visited.contains(i_vertex) {
+                    if u_vertex == i_vertex {
+                        panic!("Same node! :{}, {}", u_vertex, i_vertex);
+                    }
+                    let u_distance = u_distance_map[u_vertex];
+                    let u_pt = self.get_pressure_and_time(u_vertex, u_distance, u_cost.1, time_limit);
+                    
+                    let i_distance = i_distance_map[i_vertex];
+                    let i_pt = self.get_pressure_and_time(i_vertex, i_distance, i_cost.1, time_limit);
+
+                    if u_pt.1 > time_limit || i_pt.1 > time_limit {
+                        continue;
+                    }
+                    let mut new_visited = visited.clone();
+                    new_visited.push(u_vertex.clone());
+                    new_visited.push(i_vertex.clone());
+
+                    let u_new_cost = (u_cost.0 + u_pt.0, u_pt.1);
+                    let i_new_cost = (i_cost.0 + i_pt.0, i_pt.1);
+                    
+                    // let pair = sort_pair(u_vertex, i_vertex);
+                    let pair = (u_vertex.clone(), i_vertex.clone());
+
+                    let current_cost = node_cost_map.get_mut(&pair).unwrap();
+                    if (u_new_cost.0 + u_pt.0 + i_new_cost.0 + i_pt.0) >= current_cost.0 {
+                        *current_cost = (u_new_cost.0 + i_new_cost.0, std::cmp::max(u_new_cost.1, i_new_cost.1));
+                        queue.push_back((
+                            (u_vertex.clone(), u_new_cost),
+                            (i_vertex.clone(), i_new_cost),
+                            new_visited
+                        ));
+                    }
+                }
+            }
+            // for u_vertex in vertices.iter() {
+            //     if !visited.contains(&u_vertex) {                
+            //         for i_vertex in vertices.iter() {
+            //             if i_vertex != u_vertex && !visited.contains(&i_vertex) {                        
+            //                 let u_distance = u_distance_map[u_vertex];
+            //                 let u_pt = self.get_pressure_and_time(u_vertex, u_distance, u_cost.1, time_limit);
+                            
+            //                 let i_distance = i_distance_map[i_vertex];
+            //                 let i_pt = self.get_pressure_and_time(i_vertex, i_distance, i_cost.1, time_limit);
+
+            //                 if u_pt.1 > time_limit || i_pt.1 > time_limit {
+            //                     continue;
+            //                 }
+            //                 let mut new_visited = visited.clone();
+            //                 new_visited.push(u_vertex.clone());
+            //                 new_visited.push(i_vertex.clone());
+
+            //                 let u_new_cost = (u_cost.0 + u_pt.0, u_pt.1);
+            //                 let i_new_cost = (i_cost.0 + i_pt.0, i_pt.1);
+                            
+            //                 let pair = sort_pair(u_vertex, i_vertex);
+
+            //                 let current_cost = node_cost_map.get_mut(&pair).unwrap();
+            //                 if (u_new_cost.0 + u_pt.0 + i_new_cost.0 + i_pt.0) >= current_cost.0 {
+            //                     *current_cost = (u_new_cost.0 + i_new_cost.0, std::cmp::max(u_new_cost.1, i_new_cost.1));
+            //                     queue.push_back((
+            //                         (u_vertex.clone(), u_new_cost),
+            //                         (i_vertex.clone(), i_new_cost),
+            //                         new_visited
+            //                     ));
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
+            iteration += 1;    
+        }
+
+        
+        return (max_pressure, came_from)    
+    }        
+
+    fn get_pressure_and_time(&self, node: &String, distance: i32, time_passed: i32, time_limit: i32) -> (i32, i32) {
+        let time_to_release_pressure = time_passed + distance + 1;
+        let pressure_release = (time_limit - time_to_release_pressure) * self.map[node].flow_rate;
+        return (pressure_release, time_to_release_pressure);
+    }
+
+
+    // fn get_pressure_map(&self, distance_map: &HashMap<String, i32>, time_passed: i32) -> HashMap<String, (i32, i32)> {
+    //     let mut pressure_map: HashMap<_, _> = HashMap::new();
+    //     for (key, distance) in distance_map.iter() {
+    //         let (pressure_release, time_to_release_pressure) = self.get_pressure_and_time(key, *distance, time_passed);
+    //         if time_to_release_pressure <= 30 {
+    //             pressure_map.insert(key.clone(), (pressure_release, time_to_release_pressure));
+    //         }
+    //     }
+    //     pressure_map
+    // }
+
+    fn get_distances(&self, start: &String) -> HashMap<String, i32> {
+        let mut queue: Vec<String> = self.map.keys().map(|k| k.clone()).collect();
+        let mut prev_map: HashMap<String, Option<String>> = HashMap::new();
+        let mut distance_map: HashMap<String, i32> = HashMap::new();
+        let mut current_valve = start;
+
+        for key in queue.iter() {
+            distance_map.insert(key.clone(), i32::MAX);
+            prev_map.insert(key.clone(), None);
+        }
+        *distance_map.get_mut(current_valve).unwrap() = 0;
+
+        while queue.len() > 0 {
+            let (name, distance, pos) = distance_map
+                .iter()
+                .filter_map(|(k, v)| {
+                    if let Some(pos) = queue.iter().position(|v| v == k) {
+                        Some((k.clone(), v.clone(), pos))
+                    } else {
+                        None
+                    }
+                })
+                .min_by(|a, b| a.1.cmp(&b.1)).unwrap();
+
+            queue.remove(pos);
+
+            current_valve = &name;
+            let neighbours: Vec<_> = self.map[current_valve].leads_to.iter().filter(|n| {
+                queue.contains(n)
+            }).collect();
+
+            for neighbour in neighbours.into_iter() {
+                let alt_distance = distance + 1;
+                if alt_distance < distance_map[neighbour] {
+                    *distance_map.get_mut(neighbour).unwrap() = alt_distance;
+                    *prev_map.get_mut(neighbour).unwrap() = Some(name.clone());
+                }
+            }
+        }
+        distance_map
+    }    
+}
