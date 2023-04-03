962f849f9c5afae8acf62fc1cf1b20f6695dabd5
Martin KOŠČAK <koscak@pops.pri>
2023-04-03 16:45:17 +0200

    Minor restructure of day 18

--- src/day_18/mod.rs
+++ src/day_18/mod.rs
@@ -15,7 +15,8 @@ fn get_neighbour_coords(c: &Coord) -> Vec<Coord> {
 }
 
 pub struct BoilingBoulders {
-    droplets: Vec<Coord>
+    droplets: Vec<Coord>,
+    max_coord: Coord
 }
 
 impl crate::Advent for BoilingBoulders {
@@ -23,30 +24,37 @@ impl crate::Advent for BoilingBoulders {
         where 
             Self: Sized {                
         
-        let droplets = data.lines().map(|l| {
+        let droplets: Vec<Coord> = data.lines().map(|l| {
             let v: Vec<i8> = l.split(',').map(|n| n.parse().unwrap()).collect();
             v.try_into().unwrap()
         }).collect();
+
+        let max_x = droplets.iter().map(|d| d[0]).max().unwrap();
+        let max_y = droplets.iter().map(|d| d[1]).max().unwrap();
+        let max_z = droplets.iter().map(|d| d[2]).max().unwrap();
+        let max_coord: Coord = [max_x, max_y, max_z];
         
-        Self { droplets }
+        Self { droplets, max_coord }
     }
 
     fn part_01(&self) -> String {
-        let touching_sides_count: usize = self.droplets.iter().map(|c| {
-            let neighbour_coords = get_neighbour_coords(c);
-            self.droplets.iter().filter(|&oc| {
-                oc != c && neighbour_coords.contains(oc)
-            }).count()
-        }).sum();
-
-        println!("Touching sides: {}", touching_sides_count);
-
+        let touching_sides_count = self.touching_sides_count();
         let surface_area = self.droplets.len() * 6 - touching_sides_count;
         surface_area.to_string()
-        // 1.to_string()
     }
 
     fn part_02(&self) -> String {
         2.to_string()
+    }
+}
+
+impl BoilingBoulders {
+    fn touching_sides_count(&self) -> usize {
+        self.droplets.iter().map(|c| {
+            let neighbour_coords = get_neighbour_coords(c);
+            self.droplets.iter().filter(|&oc| {
+                oc != c && neighbour_coords.contains(oc)
+            }).count()
+        }).sum()
     }
 }
