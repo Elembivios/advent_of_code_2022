fe1e64f528333ce7f7c35dde13e68b59a52a3c69
Martin Koščak <martin.kosca@posta.si>
2023-04-13 20:48:18 +0200

    Second failed attempt.

--- src/day_19/mod.rs
+++ src/day_19/mod.rs
@@ -38,7 +38,7 @@ impl crate::Advent for NotEnoughMinerals {
         let mut factory = Factory::new(blueprint.clone());
         
         while factory.time_remaining != 0 {
-            factory.pass_minute();
+            factory.pass_minute(None);
         }
         1.to_string()
     }
@@ -104,11 +104,23 @@ impl Factory {
         }
     }
 
-    fn pass_minute(&mut self) {
+    fn pass_minute(&mut self, force_buy: Option<Mineral>) {
         println!("------------------------");
-        println!("Passed minute: {}", self.time_limit - self.time_remaining);
+        println!("Passed minute: {}", self.time_limit - self.time_remaining + 1);
+        println!("Resources: {:?}", self.minerals);
 
-        let bought_robots = self.buy_robots();
+        let mut bought_robots: HashMap<Mineral, usize> = HashMap::new();
+
+        // if let Some(mineral) = force_buy {
+        //     self.pay_for_robot(mineral);
+
+        // }
+
+        
+        while let Some(best_to_buy) = self.get_best_robot_to_buy() {
+            *bought_robots.entry(best_to_buy).or_default() += 1;
+            self.pay_for_robot(&best_to_buy);
+        }
 
         self.mine_resources();
 
@@ -117,69 +129,126 @@ impl Factory {
         self.time_remaining -= 1;
     }
 
-    fn buy_robots(&mut self) -> HashMap<Mineral, usize> {
-        let mut robots_to_buy: HashMap<Mineral, usize> = HashMap::new();
-
-        'search_robots_to_buy: loop {
-            let can_buy_robots: Vec<Mineral> = MINERALS.into_iter().filter_map(|m| {
-                if self.can_buy_robot(&m) {
-                    Some(m)
-                } else {
-                    None
-                }
-            }).collect();     
-
-            println!("Can buy robots: {:?}", can_buy_robots);
-            if can_buy_robots.len() == 0 {
-                break 'search_robots_to_buy
+    fn get_best_robot_to_buy(&self) -> Option<Mineral> {
+        // Returns a robot if this is the right time to buy one
+        // Returns a None if we should wait
+        let can_buy_robots: Vec<Mineral> = MINERALS.into_iter().filter_map(|m| {
+            if self.can_buy_robot(&m) {
+                Some(m)
+            } else {
+                None
             }
+        }).collect();     
 
-            let best_robot_we_could_buy = self.get_best_robot_we_could_buy_with_current_income();
-            println!("Best we could buy: {:?}", best_robot_we_could_buy);
-
-            if can_buy_robots.contains(&best_robot_we_could_buy) {
-                println!("Buying best");
-                *robots_to_buy.entry(best_robot_we_could_buy).or_default() += 1;
-                self.pay_for_robot(&best_robot_we_could_buy);
-                continue 'search_robots_to_buy
-            }
-
-            let mut robots_to_buy_score: HashMap<Mineral, usize> = HashMap::new();
-
-            // Can't buy best robot yet.. Do we wait to buy it or we buy a lesser one
-            let (time, sequence) = self.get_time_and_sequence_till_we_can_buy(&best_robot_we_could_buy);
-            let number_of_robots = (self.time_remaining - time / sequence) + 1;
-            robots_to_buy_score.insert(best_robot_we_could_buy, number_of_robots);
-
-            for robot in can_buy_robots {
-                let mut factory_clone = self.clone();
-                factory_clone.mine_resources();
-                let new_robots = HashMap::from([(robot, 1)]);
-                factory_clone.add_robots(new_robots);
-                factory_clone.time_remaining -= 1;
-
-                let (time, sequence) = factory_clone.get_time_and_sequence_till_we_can_buy(&best_robot_we_could_buy);
-                let number_of_robots = (factory_clone.time_remaining  - time / sequence) + 1;
-                robots_to_buy_score.insert(robot, number_of_robots);
-            }
-            println!("Robots to buy score: {:?}", robots_to_buy_score);
-
-            let (best_to_buy, _score) = robots_to_buy_score.iter().min_by(|a, b| a.1.cmp(&b.1)).unwrap();
-
-            println!("Best to buy: {:?}", best_to_buy);
-
-            if *best_to_buy == best_robot_we_could_buy {
-                // We wait with the purchase
-                break 'search_robots_to_buy;
-            }
-
-            *robots_to_buy.entry(*best_to_buy).or_default() += 1;
-            self.pay_for_robot(&best_to_buy);                                
+        if can_buy_robots.len() == 0 {
+            return None;
         }
+
+        println!("Can buy robots: {:?}", can_buy_robots);
+
+        let best_robot_we_could_buy = self.get_best_robot_we_could_buy_with_current_income();
+        // if can_buy_robots.contains(&best_robot_we_could_buy) {
+        //     return Some(best_robot_we_could_buy);
+        // }
+        println!("Best we could buy: {:?}", best_robot_we_could_buy);
+        let mut robots_to_buy_score: HashMap<Mineral, usize> = HashMap::new();
+
+        // Do we wait to buy it or we buy a lesser one
+        robots_to_buy_score.insert(best_robot_we_could_buy, self.robot_score(best_robot_we_could_buy));
+
+        for robot in can_buy_robots.iter() {
+            let mut factory_clone = self.clone();    
+            factory_clone.pay_for_robot(robot);
+            factory_clone.mine_resources();
+            let new_robots = HashMap::from([(*robot, 1usize)]);
+            factory_clone.add_robots(new_robots);
+            factory_clone.time_remaining -= 1;
+            robots_to_buy_score.insert(*robot, factory_clone.robot_score(best_robot_we_could_buy));
+        }
+        println!("Robots to buy score: {:?}", robots_to_buy_score);
+
+        let (best_to_buy, _score) = robots_to_buy_score.iter().max_by(|a, b| {
+            let c = a.1.cmp(&b.1);
+            if c.is_eq() {
+                let pos1 = MINERALS.iter().position(|m| m == a.0);
+                let pos2 = MINERALS.iter().position(|m| m == b.0);
+                return pos1.cmp(&pos2);
+            }
+            c
+        }).unwrap();
+
+        println!("Best to buy: {:?}", best_to_buy);
+
+        if can_buy_robots.contains(best_to_buy) {
+            return Some(*best_to_buy)
+        } else {
+            
+            // We wait with the purchase
+            return None
+        }
+
         
-        println!("Robots to buy: {:?}", robots_to_buy);
-        robots_to_buy
     }
+
+    // fn buy_robots(&mut self) -> HashMap<Mineral, usize> {
+    //     let mut robots_to_buy: HashMap<Mineral, usize> = HashMap::new();
+
+    //     'search_robots_to_buy: loop {
+    //         let can_buy_robots: Vec<Mineral> = MINERALS.into_iter().filter_map(|m| {
+    //             if self.can_buy_robot(&m) {
+    //                 Some(m)
+    //             } else {
+    //                 None
+    //             }
+    //         }).collect();     
+
+    //         println!("Can buy robots: {:?}", can_buy_robots);
+    //         if can_buy_robots.len() == 0 {
+    //             break 'search_robots_to_buy
+    //         }
+
+    //         let best_robot_we_could_buy = self.get_best_robot_we_could_buy_with_current_income();
+    //         println!("Best we could buy: {:?}", best_robot_we_could_buy);
+
+    //         if can_buy_robots.contains(&best_robot_we_could_buy) {
+    //             println!("Buying best");
+    //             *robots_to_buy.entry(best_robot_we_could_buy).or_default() += 1;
+    //             self.pay_for_robot(&best_robot_we_could_buy);
+    //             continue 'search_robots_to_buy
+    //         }
+
+    //         let mut robots_to_buy_score: HashMap<Mineral, usize> = HashMap::new();
+
+    //         // Can't buy best robot yet.. Do we wait to buy it or we buy a lesser one
+    //         robots_to_buy_score.insert(best_robot_we_could_buy, self.robot_score(best_robot_we_could_buy));
+
+    //         for robot in can_buy_robots {
+    //             let mut factory_clone = self.clone();
+    //             factory_clone.pass_minute(Some(robot));
+    //             factory_clone.mine_resources();
+    //             let new_robots = HashMap::from([(robot, 1)]);
+    //             factory_clone.add_robots(new_robots);
+    //             factory_clone.time_remaining -= 1;
+    //             robots_to_buy_score.insert(best_robot_we_could_buy, factory_clone.robot_score(best_robot_we_could_buy));
+    //         }
+    //         println!("Robots to buy score: {:?}", robots_to_buy_score);
+
+    //         let (best_to_buy, _score) = robots_to_buy_score.iter().min_by(|a, b| a.1.cmp(&b.1)).unwrap();
+
+    //         println!("Best to buy: {:?}", best_to_buy);
+
+    //         if *best_to_buy == best_robot_we_could_buy {
+    //             // We wait with the purchase
+    //             break 'search_robots_to_buy;
+    //         }
+
+    //         *robots_to_buy.entry(*best_to_buy).or_default() += 1;
+    //         self.pay_for_robot(&best_to_buy);                                
+    //     }
+        
+    //     println!("Robots to buy: {:?}", robots_to_buy);
+    //     robots_to_buy
+    // }
 
     fn get_best_robot_we_could_buy_with_current_income(&self) -> Mineral {
         for mineral in MINERALS.iter().rev() {            
@@ -229,5 +298,16 @@ impl Factory {
 
         let sequence_we_can_buy = (prices[mineral] + self.robots[mineral] - 1) / self.robots[mineral];
         (time_till_we_can_buy as usize, sequence_we_can_buy)
+    }
+
+    fn robot_score(&self, mineral: Mineral) -> usize {
+        let (time, sequence) = self.get_time_and_sequence_till_we_can_buy(&mineral);
+        println!("m: {:?}, t: {}, s: {}, tr: {}", mineral, time, sequence, self.time_remaining);
+        if time > self.time_remaining {
+            return 0;
+        }
+        (self.time_remaining - time) / sequence + 1
+        // let number_of_robots = (self.time_remaining + time / sequence) + 1;
+        // number_of_robots
     }
 }
