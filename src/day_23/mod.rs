7ebd40b145e3c6ee6c39232341112d1919bc9e15
Martin KOSCAK <martin.koscak@posta.si>
2023-07-27 14:46:25 +0200

    Added solution for day 23.

--- src/day_23/mod.rs
+++ src/day_23/mod.rs
@@ -1,12 +1,9 @@
-use crate::utils::point::{Coord, Point, Direction, DIRECTIONS};
-use crate::utils::wait_user_input;
+use crate::utils::point::{Coord, Direction, DIRECTIONS};
 use std::collections::HashMap;
 
-
-type Elf = Point<i32, Direction>;
 type C = Coord<i32>;
 pub struct UnstableDiffusion {
-    elfs: Vec<Elf>
+    elfs: Vec<C>
 }
 
 impl crate::Advent for UnstableDiffusion {
@@ -14,10 +11,10 @@ impl crate::Advent for UnstableDiffusion {
         where 
             Self: Sized {
         let height = data.lines().count() as i32;
-        let elfs: Vec<Elf> = data.lines().enumerate().map(|(y, l)| {
+        let elfs: Vec<C> = data.lines().enumerate().map(|(y, l)| {
             l.chars().enumerate().filter_map(|(x, c)| {
                 match c {
-                    '#' => Some(Point::new(x as i32, height - 1 - y as i32, Direction::E)),
+                    '#' => Some(C::new(x as i32, height - 1 - y as i32)),
                     _ => None
                 }
             }).collect::<Vec<_>>()
@@ -27,34 +24,116 @@ impl crate::Advent for UnstableDiffusion {
     }
 
     fn part_01(&self) -> String {
+        let (_rounds, elfs) = self.move_elfs(Some(10));
+        UnstableDiffusion::count_empty(&elfs).to_string()
+    }
+
+    fn part_02(&self) -> String {
+        let (rounds, _elfs) = self.move_elfs(None);
+        (rounds + 1).to_string()
+    }
+}
+
+impl UnstableDiffusion {
+    #[allow(dead_code)]
+    fn display_elfs(elfs: &Vec<C>) {
+        let mut max_x = i32::MIN;
+        let mut min_x = 0;        
+        let mut max_y = i32::MIN;
+        let mut min_y = 0;
+        for coord in elfs {
+            if coord.x < min_x {
+                min_x = coord.x;
+            } 
+            if coord.x > max_x {
+                max_x = coord.x;
+            }
+            if coord.y < min_y {
+                min_y = coord.y;
+            } 
+            if coord.y > max_y {
+                max_y = coord.y;
+            }            
+        }
+        println!("X: {} -> {}, Y: {} -> {}", min_x, max_x, min_y, max_y);
+
+        for y in (min_y..=max_y).rev() {
+            let mut row: String = String::new();
+            for x in min_x..=max_x {                
+                if elfs.iter().any(|c: &Coord<i32>| c.x == x && c.y == y) {
+                    row.push('#');
+                } else {
+                    row.push('.');
+                }
+                
+            }
+            println!("{}", row);
+        }
+    }
+
+    fn count_empty(elfs: &Vec<C>) -> usize {
+        let mut max_x = i32::MIN;
+        let mut min_x = i32::MAX;        
+        let mut max_y = i32::MIN;
+        let mut min_y = i32::MAX;
+        for coord in elfs {
+            if coord.x < min_x {
+                min_x = coord.x;
+            } 
+            if coord.x > max_x {
+                max_x = coord.x;
+            }
+            if coord.y < min_y {
+                min_y = coord.y;
+            } 
+            if coord.y > max_y {
+                max_y = coord.y;
+            }            
+        }
+        let mut empty_counter = 0;
+        for y in (min_y..=max_y).rev() {
+            for x in min_x..=max_x {                
+                if !elfs.iter().any(|c| c.x == x && c.y == y) {
+                    empty_counter += 1;
+                }                
+            }
+        }
+
+        empty_counter
+    }
+
+    fn move_elfs(&self, round_limit: Option<usize>) -> (usize, Vec<C>) {
         const DIRECTION_ORDER: [Direction; 4] = [Direction::N, Direction::S, Direction::W, Direction::E];
-        let mut elfs = self.elfs.clone();        
-        for i in 0..10 {
-            println!("{} -- Elfs: {:?}", i, elfs);
-            UnstableDiffusion::display_elfs(&elfs);
+        let mut elfs = self.elfs.clone();
+        let mut i = 0;
+        'move_elfs: loop {
+            match round_limit {
+                Some(rl) => if i >= rl { break; },
+                None => {}
+            }
+            let dir_pos = i % DIRECTION_ORDER.len();
+            // println!("{} -- Elfs: {:?}", i, elfs);
+            // UnstableDiffusion::display_elfs(&elfs);
             let mut moves: HashMap<usize, Direction> = HashMap::new();
             'check_moves: for (elf_i, elf) in elfs.iter().enumerate() {
-                let neigbour_elfs: Vec<Option<&Elf>> = DIRECTIONS
+                let neigbour_elfs: Vec<Option<&C>> = DIRECTIONS
                     .iter()
                     .map(|direction| {
-                        let nc = elf.coord.get_neighbour(direction);
-                        elfs.iter().filter(|p| p.coord == nc).next()
+                        let nc = elf.get_neighbour(direction);
+                        elfs.iter().filter(|c| **c == nc).next()
                     }).collect();                
     
                 // No need to move if all spaces are empty 
                 if neigbour_elfs.iter().all(|n| n.is_none()) {
                     continue 'check_moves;
-                }        
-                let last_dir = elf.value;
-                let dir_pos = DIRECTION_ORDER.iter().position(|d| *d == last_dir).unwrap();
-    
-                'search_direction: for new_dir in DIRECTION_ORDER.iter().cycle().skip(dir_pos + 1).take(DIRECTION_ORDER.len()) {
-                    let facing_spaces: [Option<&Elf>; 3] = match new_dir {
+                }
+                'search_direction: for new_dir in DIRECTION_ORDER.iter().cycle().skip(dir_pos).take(DIRECTION_ORDER.len()) {
+                    let facing_spaces: [Option<&C>; 3] = match new_dir {
                         Direction::N => {[neigbour_elfs[7], neigbour_elfs[0], neigbour_elfs[1]]},
                         Direction::S => {[neigbour_elfs[3], neigbour_elfs[4], neigbour_elfs[5]]},
                         Direction::E => {[neigbour_elfs[1], neigbour_elfs[2], neigbour_elfs[3]]},
                         Direction::W => {[neigbour_elfs[5], neigbour_elfs[6], neigbour_elfs[7]]},
-                        _ => panic!("Invalid dir {:?}", last_dir)
+                        _ => panic!("Invalid dir {:?}", dir_pos)
                     };
                     if facing_spaces.iter().all(|s| s.is_none()) {
                         moves.insert(elf_i, *new_dir);                        
@@ -63,12 +142,15 @@ impl crate::Advent for UnstableDiffusion {
                 }
             }
 
+            if moves.len() == 0 {
+                break 'move_elfs;
+            }
+
             let mut moves_to: HashMap<usize, C> = HashMap::with_capacity(moves.len());
             for (elf_i, new_dir) in moves.iter() {
                 let elf = elfs.get_mut(*elf_i).unwrap();
-                let new_coord = elf.coord.get_neighbour(new_dir);
+                let new_coord = elf.get_neighbour(new_dir);
                 moves_to.insert(*elf_i, new_coord);
-                elf.value = *new_dir;
             }
 
             let mut target_count: HashMap<C, usize> = HashMap::with_capacity(moves_to.len());
@@ -92,50 +174,11 @@ impl crate::Advent for UnstableDiffusion {
                     !duplicated_targets.contains(target_coord)
                 }).for_each(|(elf_i, target_coord)| {
                     let elf = elfs.get_mut(elf_i).unwrap();
-                    elf.coord = target_coord;
+                    *elf = target_coord;
                 });
-        }        
-        1.to_string()
-    }
 
-    fn part_02(&self) -> String {
-        2.to_string()
-    }
-}
-
-impl UnstableDiffusion {
-    fn display_elfs(elfs: &Vec<Elf>) {
-        let mut max_x = i32::MIN;
-        let mut min_x = 0;        
-        let mut max_y = i32::MIN;
-        let mut min_y = 0;
-        for elf in elfs {
-            if elf.coord.x < min_x {
-                min_x = elf.coord.x;
-            } 
-            if elf.coord.x > max_x {
-                max_x = elf.coord.x;
-            }
-            if elf.coord.y < min_y {
-                min_y = elf.coord.y;
-            } 
-            if elf.coord.y > max_y {
-                max_y = elf.coord.y;
-            }            
+            i += 1;
         }
-        println!("X: {} -> {}, Y: {} -> {}", min_x, max_x, min_y, max_y);
-
-        for y in (min_y..=max_y).rev() {
-            let mut row: String = String::new();
-            for x in min_x..=max_x {                
-                if elfs.iter().any(|p| p.coord.x == x && p.coord.y == y) {
-                    row.push('#');
-                } else {
-                    row.push('.');
-                }
-                
-            }
-            println!("{}", row);
-        }
+        (i, elfs)
     }
 }
