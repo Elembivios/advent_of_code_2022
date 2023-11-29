cf1d61a968810a4ddbb4fec080fec22d4c64d4b4
Martin KOSCAK <martin.koscak@posta.si>
2023-11-29 09:43:07 +0100

    Work in progress for day 24

--- src/day_24/mod.rs
+++ src/day_24/mod.rs
@@ -1,7 +1,9 @@
 
 use crate::utils::point::{Point, Coord, Direction};
+use core::fmt;
 
 type Blizzard = Point<usize, Direction>;
+
 pub struct BlizardBasin {
     height: usize,
     width: usize,
@@ -51,4 +53,39 @@ impl crate::Advent for BlizardBasin {
     fn part_02(&self) -> String {
         2.to_string()
     }
+}
+
+impl BlizardBasin {
+    fn sort_blizzards(&self, &mut mut blizzards: &mut Vec<Blizzard>) {
+        blizzards.sort_unstable_by(|a, b| {
+            a.coord.cmp(&b.coord)
+        });        
+    }
+}
+
+impl fmt::Display for BlizardBasin
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "\n")?;
+        let mut blizzards = self.blizzards.clone();
+        self.sort_blizzards(&mut blizzards);        
+        
+        let mut blizzards_it = blizzards.iter();
+
+        for y in 0..self.height {            
+            for x in 0..self.width {
+                let c = Coord::new(x, y);            
+                let current_coord_blizards: Vec<&Blizzard> = blizzards_it.take_while(|b| {
+                    b.coord == c
+                }).collect();
+                match current_coord_blizards.len() {
+                    0 => write!(f, ".")?,
+                    1 => write!(f, "{}", current_coord_blizards[0].value)?,
+                    n => write!(f, "{}", n)?
+                }
+            }
+            write!(f, "\n")?;
+        }
+        write!(f, "\n")
+    }    
 }
